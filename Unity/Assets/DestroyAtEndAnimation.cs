877dfca59517c3965ba4c718f27f2d9e4fdf61ce
Alexandre MARIE <alexandre_marie@outlook.com>
2017-01-18 15:55:46 +0100

    Ajout des fondu logo studio + game title

---
+++ Unity/Assets/DestroyAtEndAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAtEndAnimation : MonoBehaviour
+{
+
+    public void DestroyAtEvent()
+    {
+        Destroy(this.gameObject);
+    }
+}
