4e945c6e5f216bdd11506e5e2ffebe1368eebdb4
Ichi Sumeragi <ichisumeragi@hotmail.fr>
2017-01-18 16:34:23 +0100

    Commit pattern
    Commit pattern Burst

--- Unity/Assets/_scripts/Patterns.cs
+++ Unity/Assets/_scripts/Patterns.cs
@@ -10,7 +10,7 @@ public class Patterns : MonoBehaviour
     [SerializeField]
     private Transform emitter;
 
-    private int bulletQuantity = 500; //Quantity of bullets to fire
+    private int bulletQuantity = 20; //Quantity of bullets to fire
     private int floret = 1; //Number of the current floret
     private float divergence = 137.5f; //Divergence angle
     private float bulletDelay = 0.0005f; //Delay between bullets
@@ -23,20 +23,37 @@ public class Patterns : MonoBehaviour
 
     IEnumerator StartPattern()
     {
-        StartCoroutine(Phyllotaxis(emitter, bullet, divergence, floret, bulletQuantity));
+        //StartCoroutine(Phyllotaxis(emitter, bullet, divergence, floret, bulletQuantity));
+        StartCoroutine(Burst(emitter, bullet, bulletQuantity));
         yield return new WaitForSeconds(2.0f);
     }
 
     private IEnumerator Phyllotaxis(Transform emitter, Transform bullet, float divergence, int floret, int bulletQuantity)
     {
-        for (int i = 0; i <= bulletQuantity; i++)
+        for (int i = 0; i < bulletQuantity; i++)
         {
-            //φ = n ∗ 137.5◦ r = c√n
             float phi = floret * divergence;
 
             Instantiate(bullet, emitter.position, Quaternion.Euler(0, phi, 0));
+
             floret++;
+
             yield return new WaitForSeconds(bulletDelay);
         }   
+    }
+
+    private IEnumerator Burst(Transform emitter, Transform bullet, int bulletQuantity)
+    {
+        float additiveAngle = 360.0f / bulletQuantity;
+        float phi = 0.0f;
+
+        for (int i = 0; i < bulletQuantity; i++)
+        {
+            Instantiate(bullet, emitter.position, Quaternion.Euler(0, phi, 0));
+
+            phi += additiveAngle;
+        }
+
+        yield return new WaitForSeconds(2.0f);
     }
 }
