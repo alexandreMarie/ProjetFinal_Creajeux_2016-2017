b53284b739043f55df17256fad48779dd4877f9a
IchiSumeragi <ichisumeragi@hotmail.fr>
2017-01-19 09:07:25 +0100

    Update Patterns
    Ajout des patterns de burst et de phyllotaxis ouverts

--- Unity/Assets/_scripts/Patterns.cs
+++ Unity/Assets/_scripts/Patterns.cs
@@ -7,8 +7,8 @@ public class Patterns : MonoBehaviour
     [SerializeField]
     private Transform bullet = null;
 
-    private int bulletQuantityPhyllotaxis = 500; //Quantity of bullets to fire
-    private int bulletQuantityBurst = 50;
+    private int bulletQuantityPhyllotaxis = 250; //Quantity of bullets to fire for the phyllotaxis
+    private int bulletQuantityBurst = 50; //Quantity of bullets to fire for the bursts
     private float divergence = 137.5f; //Divergence angle
     private float bulletDelay = 0.005f; //Delay between bullets
 
@@ -19,8 +19,15 @@ public class Patterns : MonoBehaviour
 
     IEnumerator StartPattern()
     {
+        StartCoroutine(Phyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(Burst (bullet, bulletQuantityBurst));
+        yield return new WaitForSeconds(4.0f);
         StartCoroutine(OpenPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
-        //StartCoroutine(Burst(emitter, bullet, bulletQuantityBurst));
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(OpenBurst(bullet, bulletQuantityBurst));
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(FocusedPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
         yield return new WaitForSeconds(2.0f);
     }
 
@@ -36,14 +43,30 @@ public class Patterns : MonoBehaviour
         }
     }
 
-    private IEnumerator Burst(Transform emitter, Transform bullet, int bulletQuantity)
+    private IEnumerator Burst(Transform bullet, int bulletQuantity)
     {
         float additiveAngle = 360.0f / bulletQuantity;
         float phi = 0.0f;
 
         for (int i = 0; i < bulletQuantity; i++)
         {
-            Instantiate(bullet, emitter.position, Quaternion.Euler(0, phi, 0));
+            Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
+
+            phi += additiveAngle;
+        }
+
+        yield return new WaitForSeconds(2.0f);
+    }
+
+    private IEnumerator OpenBurst(Transform bullet, int bulletQuantity)
+    {
+        float additiveAngle = 360.0f / bulletQuantity;
+        float phi = 0.0f;
+
+        for (int i = 0; i < bulletQuantity; i++)
+        {
+            if ((phi % 360 < 100) || (phi % 360 > 170))
+                Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
 
             phi += additiveAngle;
         }
@@ -68,7 +91,7 @@ public class Patterns : MonoBehaviour
         for (int floret = 0; floret < bulletQuantity; floret++)
         {
             float phi = floret * divergence;
-            if ((phi % 360 < 150) && (phi % 360 > 170))
+            if ((phi % 360 < 100) || (phi % 360 > 170))
                 Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
 
             yield return new WaitForSeconds(bulletDelay);
