c1379fa9f51db4cd54aa3e8fe964e87c400b245b
IchiSumeragi <ichisumeragi@hotmail.fr>
2017-01-20 13:25:35 +0100

    Commit patterns
    Ajout des input pour tester les patterns + réorganisation du code des
    patterns + ajout de la phyllotaxis rotative + ajout du chain burst
    rotatif

--- Unity/Assets/_scripts/Patterns.cs
+++ Unity/Assets/_scripts/Patterns.cs
@@ -7,28 +7,60 @@ public class Patterns : MonoBehaviour
     [SerializeField]
     private Transform bullet = null;
 
-    private int bulletQuantityPhyllotaxis = 250; //Quantity of bullets to fire for the phyllotaxis
-    private int bulletQuantityBurst = 50; //Quantity of bullets to fire for the bursts
+    private int bulletQuantityPhyllotaxis = 500; //Quantity of bullets to fire for the phyllotaxis
+    private int bulletQuantityBurst = 30; //Quantity of bullets to fire for the bursts
     private float divergence = 137.5f; //Divergence angle
     private float bulletDelay = 0.005f; //Delay between bullets
 
     void Start()
     {
-        StartCoroutine(StartPattern());
+        //StartCoroutine(StartPattern());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            StartCoroutine(Phyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            StartCoroutine(OpenPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            StartCoroutine(FocusedPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            StartCoroutine(Burst(bullet, bulletQuantityBurst));
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            StartCoroutine(OpenBurst(bullet, bulletQuantityBurst, i1, i2));
     }
 
     IEnumerator StartPattern()
     {
-        StartCoroutine(Phyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
-        yield return new WaitForSeconds(2.0f);
-        StartCoroutine(Burst (bullet, bulletQuantityBurst));
-        yield return new WaitForSeconds(4.0f);
-        StartCoroutine(OpenPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
-        yield return new WaitForSeconds(5.0f);
-        StartCoroutine(OpenBurst(bullet, bulletQuantityBurst));
-        yield return new WaitForSeconds(2.0f);
-        StartCoroutine(FocusedPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
-        yield return new WaitForSeconds(2.0f);
+        //int i1 = 100;
+        //int i2 = 170;
+
+        //StartCoroutine(Phyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+        //yield return new WaitForSeconds(2.0f);
+
+        //StartCoroutine(Burst(bullet, bulletQuantityBurst));
+        //yield return new WaitForSeconds(4.0f);
+
+        //StartCoroutine(OpenPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+        //yield return new WaitForSeconds(5.0f);
+
+        //for (int i = 0; i < 30; i++)
+        //{
+        //    StartCoroutine(OpenBurst(bullet, bulletQuantityBurst, i1, i2));
+        //    i1 += 5;
+        //    i2 += 5;
+        //    yield return new WaitForSeconds(0.01f);
+        //}
+
+        //StartCoroutine(FocusedPhyllotaxis(bullet, divergence, bulletQuantityPhyllotaxis));
+        //yield return new WaitForSeconds(2.0f);
+
+        yield return new WaitForSeconds(1.0f);
     }
 
     private IEnumerator Phyllotaxis(Transform bullet, float divergence, int bulletQuantity)
@@ -38,6 +70,37 @@ public class Patterns : MonoBehaviour
             float phi = floret * divergence;
 
             Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
+
+            yield return new WaitForSeconds(bulletDelay);
+        }
+    }
+
+    private IEnumerator FocusedPhyllotaxis(Transform bullet, float divergence, int bulletQuantity)
+    {
+        for (int floret = 0; floret < bulletQuantity; floret++)
+        {
+            float phi = floret * divergence;
+            if ((phi % 360 > 200) && (phi % 360 < 250))
+                Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
+
+            yield return new WaitForSeconds(bulletDelay);
+        }
+    }
+
+    private IEnumerator OpenPhyllotaxis(Transform bullet, float divergence, int bulletQuantity)
+    {
+        int i1 = 100;
+        int i2 = 170;
+
+        for (int floret = 0; floret < bulletQuantity; floret++)
+        {
+            float phi = floret * divergence;
+            if ((phi % 360 < i1) || (phi % 360 > i2))
+            {
+                Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
+                //i1 += 1;
+                //i2 += 1;
+            }
 
             yield return new WaitForSeconds(bulletDelay);
         }
@@ -58,43 +121,20 @@ public class Patterns : MonoBehaviour
         yield return new WaitForSeconds(2.0f);
     }
 
-    private IEnumerator OpenBurst(Transform bullet, int bulletQuantity)
+    private IEnumerator OpenBurst(Transform bullet, int bulletQuantity, int i1, int i2)
     {
         float additiveAngle = 360.0f / bulletQuantity;
         float phi = 0.0f;
-
         for (int i = 0; i < bulletQuantity; i++)
         {
-            if ((phi % 360 < 100) || (phi % 360 > 170))
+            if ((phi % 360 < i1) || (phi % 360 > i2))
+            {
                 Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
+            }
 
             phi += additiveAngle;
         }
 
         yield return new WaitForSeconds(2.0f);
-    }
-
-    private IEnumerator FocusedPhyllotaxis(Transform bullet, float divergence, int bulletQuantity)
-    {
-        for (int floret = 0; floret < bulletQuantity; floret++)
-        {
-            float phi = floret * divergence;
-            if ((phi % 360 > 200) && (phi % 360 < 250))
-                Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
-
-            yield return new WaitForSeconds(bulletDelay);
-        }
-    }
-
-    private IEnumerator OpenPhyllotaxis(Transform bullet, float divergence, int bulletQuantity)
-    {
-        for (int floret = 0; floret < bulletQuantity; floret++)
-        {
-            float phi = floret * divergence;
-            if ((phi % 360 < 100) || (phi % 360 > 170))
-                Instantiate(bullet, transform.position, Quaternion.Euler(0, phi, 0));
-
-            yield return new WaitForSeconds(bulletDelay);
-        }
     }
 }
