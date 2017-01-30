246bfeed62d6b9d9a531bd8a861f6e96e579f1d2
IchiSumeragi <ichisumeragi@hotmail.fr>
2017-01-30 17:49:14 +0100

    Commit player

---
+++ Unity/Assets/_scripts/Add_Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Add_Bullet : MonoBehaviour {
+
+    public GameObject Bullet;
+    Transform Player_Transform;
+
+	// Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    public void Shoot(float Rotation)
+    {
+        //Transform  Temps_Transform = m_Tr;
+        Debug.Log("Test");
+        //Transform Temp_Transforme = Player_Transform;
+        Quaternion Quater_Bullet = transform.rotation;
+        Quater_Bullet.y = -0;
+        Instantiate(Bullet, transform.position, Quaternion.Euler(-90, Rotation, 0));
+    }
+
+    public void Special_Attack(int NumberBullets)
+    {
+        int Degrees = 360 / NumberBullets;
+        for(int i = 0;i < NumberBullets ; i++)
+        {
+            Instantiate(Bullet, transform.position, Quaternion.Euler(-90, Degrees * i, 0));
+        }
+    }
+}
--- Unity/Assets/_scripts/bulletScript.cs
+++ Unity/Assets/_scripts/bulletScript.cs
@@ -21,7 +21,9 @@ public class bulletScript : MonoBehaviour
     }
     void Update()
     {
+        float i = Random.value;
         transform.position += transform.forward * speed * Time.deltaTime;
+        transform.position.Set(0, Mathf.Cos(i), 0);
     }
 
     void OnCollisionEnter(Collision col)
