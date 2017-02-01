72d4342cdd10bda8eb65d479f6d24a33f04f022d
Alexandre MARIE <alexandre_marie@outlook.com>
2017-02-01 18:04:51 +0100

    Dialog Parser terminé + Debut Dialog Manager

--- Unity/Assets/_scripts/DialogParser.cs
+++ Unity/Assets/_scripts/DialogParser.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System;
 
 public class DialogParser : MonoBehaviour
 {
@@ -36,6 +37,92 @@ public class DialogParser : MonoBehaviour
         string file = "Assets/Data/Dialog";
         //string sceneNum = EditorApplication.currentScene; 
         string sceneNum = EditorSceneManager.GetActiveScene().name;
+        sceneNum = Regex.Replace(sceneNum, "[^0-9]", "");
+        file += sceneNum;
+        file += ".txt";
 
+        lines = new List<DialogLine>();
+
+        LoadDialog(file);
+
+    }
+
+    private void LoadDialog(string filename)
+    {
+        string line = "";
+        StreamReader r = new StreamReader(filename);
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    string[] lineData = line.Split(';');
+                    if (lineData[0] == "Player")
+                    {
+                        DialogLine lineEntry = new DialogLine(lineData[0], "", 0, "");
+                        lineEntry.options = new string[lineData.Length - 1];
+                        for (int i = 1; i < lineData.Length; i++)
+                        {
+                            lineEntry.options[i - 1] = lineData[i];
+                        }
+                        lines.Add(lineEntry);
+                    }
+                    else
+                    {
+                        DialogLine lineEntry = new DialogLine(lineData[0], lineData[1],int.Parse(lineData[2]), lineData[3]);
+                        lines.Add(lineEntry);
+                    }
+                }
+            } while (line != string.Empty);
+            r.Close();
+        }
+    }
+
+    public string GetPosition(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].position;
+        }
+        return "";
+    }
+
+    public string GetName(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].name;
+        }
+        return "";
+    }
+
+    public string GetContent(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].content;
+        }
+        return "";
+    }
+
+    public int GetPose(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].pose;
+        }
+        return 0;
+    }
+
+    public string[] GetOptions(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].options;
+        }
+        return new string[0];
     }
 }
