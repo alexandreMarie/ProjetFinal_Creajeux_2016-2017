d8a2036e79dd504214a92441960c80d6f94c8d30
Tcartegnie <Sorakssis@gmail.com>
2017-02-20 14:35:58 +0100

    Merge_Player
    Change :
    - Fix the position of the instance of the bullet
    - Number of bullet throwed the specialn attack, was   modifiable in the
    inspector
    - Fix the Hitbox of the arena
    - Drink a coffee

--- Unity/Assets/_scripts/Add_Bullet.cs
+++ Unity/Assets/_scripts/Add_Bullet.cs
@@ -18,17 +18,19 @@ public class Add_Bullet : MonoBehaviour {
         //Transform  Temps_Transform = m_Tr;
         Debug.Log("Test");
         //Transform Temp_Transforme = Player_Transform;
+        Vector3 PosBalle = new Vector3(transform.position.x, transform.position.y + 20, transform.position.z);
         Quaternion Quater_Bullet = transform.rotation;
         Quater_Bullet.y = -0;
-        Instantiate(Bullet, transform.position, Quaternion.Euler(-90, Rotation, 0));
+        Instantiate(Bullet, PosBalle, Quaternion.Euler(-90, Rotation, 0));
     }
 
     public void Special_Attack(int NumberBullets)
     {
+        Vector3 PosBalle = new Vector3(transform.position.x, transform.position.y + 20, transform.position.z);
         int Degrees = 360 / NumberBullets;
         for(int i = 0;i < NumberBullets ; i++)
         {
-            Instantiate(Bullet, transform.position, Quaternion.Euler(-90, Degrees * i, 0));
+            Instantiate(Bullet,PosBalle, Quaternion.Euler(-90, Degrees * i, 0));
         }
     }
 }
