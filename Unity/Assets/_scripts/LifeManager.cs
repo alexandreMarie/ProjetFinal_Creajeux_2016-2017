c26fa5637012934ee706c1e2abb2a5ef17f90687
Alexandre MARIE <alexandre_marie@outlook.com>
2017-02-20 15:06:14 +0100

    Ajout de la lifebar du joueur

---
+++ Unity/Assets/_scripts/LifeManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LifeManager : MonoBehaviour
+{
+    Image imgFilledBar;
+
+    public void Start()
+    {
+        // Récup de l'image configurée en filled
+        imgFilledBar = GetComponentInChildren<Image>();
+        if (imgFilledBar == null)
+        {
+            Debug.Log("Aucune image trouvée");
+        }
+    }
+
+    /// <summary>
+    /// MAJ de la lifebar du joueur
+    /// </summary>
+    /// <param name="_maxValue">Nombre de pdv max du joueur</param>
+    /// <param name="_value">Nombre de pdv restants du joueur</param>
+    public void UpdateLifeBar(int _maxValue, int _value)
+    {
+        // MAJ de l'UI seulement quand appelée
+        if (imgFilledBar != null)
+        {
+            imgFilledBar.fillAmount = _value / _maxValue;
+        }
+    }
+}
