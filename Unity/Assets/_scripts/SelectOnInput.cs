a5aa4480e94af60d0ced23bc255de28ba850caa2
Alexandre MARIE <alexandre_marie@outlook.com>
2017-03-06 13:59:15 +0100

    ajout des  sons et navigation a la manettes dans le menu

--- Unity/Assets/_scripts/SelectOnInput.cs
+++ Unity/Assets/_scripts/SelectOnInput.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class SelectOnInput : MonoBehaviour {
 
+    [SerializeField]
+    EventSystem eventSystem;
+
+    [SerializeField]
+    GameObject goSelected; // the actually selected game object
+
+    bool buttonSelected = false; // to "activate the right panel to navigate
+
 	// Use this for initialization
 	void Start () {
-	
+            eventSystem.SetSelectedGameObject(goSelected);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(goSelected);
+            buttonSelected = true;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
 }
