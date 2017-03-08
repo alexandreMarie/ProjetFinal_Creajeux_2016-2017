c5ea9eb214ed56d7c7461afe5a8f1cccbf6e2266
Alexandre MARIE <alexandre_marie@outlook.com>
2017-03-08 15:05:44 +0100

    Ajout du controle de l'intensité de la lumiére + MAJ du prefab light

---
+++ Unity/Assets/_scripts/LightTester.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTester : MonoBehaviour {
+
+    public LightsController lc;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (lc.TurnLight == true)
+            {
+                // les lumieres sont allumées
+                lc.TurnLight = false;
+            }
+            else
+            {
+                lc.TurnLight = true;
+            }
+        }	
+	}
+}
---
+++ Unity/Assets/_scripts/LightsController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightsController : MonoBehaviour
+{
+
+    [SerializeField]
+    AnimationCurve lightBehaviour;
+
+    [SerializeField]
+    float timer;
+
+    [SerializeField]
+    float duration = 1.5f;
+
+    Light[] lights;
+
+    bool turnLight = true;
+
+    /// <summary>
+    /// Allume ou éteint des lumieres
+    /// </summary>
+    public bool TurnLight
+    {
+        get
+        {
+            return turnLight;
+        }
+
+        set
+        {
+            turnLight = value;
+            //timer = 0f;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        lights = GetComponentsInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = Mathf.Clamp(timer + (turnLight == true ? 1 : -1) * Time.unscaledDeltaTime / duration, 0f, 1f);
+        lights[1].intensity = Mathf.Lerp(0f, 8f, lightBehaviour.Evaluate(timer));
+    }
+}
