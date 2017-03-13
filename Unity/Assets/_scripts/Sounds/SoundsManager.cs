88ea2b41a6342cd491ed7647ef28b53ecdde13db
boobasparte <remy.carivenc@hotmail.fr>
2017-03-13 18:12:10 +0100

    GameOver

---
+++ Unity/Assets/_scripts/Sounds/SoundsManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundsManager : MonoBehaviour {
+
+
+    private static SoundsManager instance = null;
+
+    [SerializeField]
+    private AudioSource sourceMusic;
+
+    [SerializeField]
+    private List<AudioClip> menuMusic;
+    [SerializeField]
+    private List<AudioClip> ambientMusic;
+    [SerializeField]
+    private List<AudioClip> sfxMusic;
+
+    [SerializeField]
+    private float volumeMenuMusic = 1.0f;
+
+    [SerializeField]
+    private float volumeAmbientMusic = 1.0f;
+
+    [SerializeField]
+    private float volumeSFX = 1.0f;
+
+    public static SoundsManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public float VolumeMenuMusic
+    {
+        get
+        {
+            return volumeMenuMusic;
+        }
+
+        set
+        {
+            volumeMenuMusic = value;
+        }
+    }
+
+    public float VolumeAmbientMusic
+    {
+        get
+        {
+            return volumeAmbientMusic;
+        }
+
+        set
+        {
+            volumeAmbientMusic = value;
+            sourceMusic.volume = volumeAmbientMusic;
+        }
+    }
+
+    public float VolumeSFX
+    {
+        get
+        {
+            return volumeSFX;
+        }
+
+        set
+        {
+            volumeSFX = value;
+        }
+    }
+
+    public List<AudioClip> SfxMusic
+    {
+        get
+        {
+            return sfxMusic;
+        }
+        
+    }
+
+    public List<AudioClip> AmbientMusic
+    {
+        get
+        {
+            return ambientMusic;
+        }
+    }
+
+    public List<AudioClip> MenuMusic
+    {
+        get
+        {
+            return menuMusic;
+        }
+    }
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+
+            sourceMusic.clip = sfxMusic[0];
+           // sourceMusic.Play();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Unity/Assets/_scripts/Static/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance = null;
+
+    public bool dead = false;
+    private GameObject gameOver;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("GameManager").AddComponent<GameManager>();
+                DontDestroyOnLoad(instance);
+            }
+            return instance;
+        }
+    }
+    void Start()
+    {
+        gameOver = GameObject.FindGameObjectWithTag("GameOver");
+    }
+
+    void Update()
+    {
+       
+        if(dead)
+        {
+            gameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            gameOver.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public bool Dead
+    {
+        get
+        {
+            return dead;
+        }
+
+        set
+        {
+            dead = value;
+        }
+    }
+
+}
