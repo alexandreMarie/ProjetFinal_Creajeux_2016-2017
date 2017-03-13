d62ca7c800cc26107933605896f0b3d8c9c326d9
Alexandre MARIE <alexandre_marie@outlook.com>
2017-03-13 16:45:29 +0100

    Ajout du Bloom

---
+++ Unity/Assets/BloomTester.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloomTester : MonoBehaviour {
+
+    public BloomController bc;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (bc.TurnBloom == true)
+            {
+                bc.TurnBloom = false;
+            }
+            else
+            {
+                bc.TurnBloom = true;
+            }
+        }
+	}
+}
