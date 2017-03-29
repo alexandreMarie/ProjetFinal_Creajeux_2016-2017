f5f1fbeafa84777b97d4ea608c5c74694a351bf1
Ichi Sumeragi <ichisumeragi@hotmail.fr>
2017-03-29 14:43:22 +0200

    Commit cleanup

--- Unity/Assets/_scripts/Static/GameManager.cs
+++ Unity/Assets/_scripts/Static/GameManager.cs
@@ -66,7 +66,7 @@ public class GameManager : MonoBehaviour {
             {
            // boss.GetComponent<LilithAI>().LilithAccessor.StopAllCoroutines();
             //boss.GetComponent<LilithAI>().StopAllCoroutines();
-            players[0].GetComponent<Player>().enabled = false;
+            players[0].GetComponent<Horsemen>().enabled = false;
             players[0].GetComponent<Add_Bullet>().enabled = false;
             //players[1].GetComponent<Player>().enabled = false;
             //players[1].GetComponent<Add_Bullet>().enabled = false;
