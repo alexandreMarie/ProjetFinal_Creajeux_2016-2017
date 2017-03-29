0b468b58c65e63fa8382c0fa12bb6414583eb55f
Alexandre MARIE <alexandre_marie@outlook.com>
2017-03-29 13:49:46 +0200

    Update bullets.cs

--- Unity/Assets/_scripts/Bullets/Bullet.cs
+++ Unity/Assets/_scripts/Bullets/Bullet.cs
@@ -8,15 +8,15 @@ public class Bullet : MonoBehaviour
     private float speed = 10.0f;
 
     [SerializeField]
-    [Range(1.0f, 20.0f)]
+    [Range(1.0f, 10.0f)]
     private float destroyTime = 5.0f;
     #endregion
 
+    private bool rotateBullet = false;
     private float lerpFactor = 0.0f;
 
     private Vector3 rotation = new Vector3(0.0f, 0.0f, 0.0f);
-    
-    private bool rotateBullet = false;
+
     public bool RotateBullet
     {
         get { return rotateBullet; }
@@ -40,30 +40,6 @@ public class Bullet : MonoBehaviour
             rotation = new Vector3(0, Mathf.Lerp(0, 180, lerpFactor), 0);
 
             transform.Rotate(rotation);
-        }
-    }
-
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.tag == "Wall")
-            Destroy(gameObject, 0.1f);
-
-        if (col.tag == "Player")
-        {
-            Player player = col.GetComponent<Player>();
-
-            if (player.Get_Invicible() == false)
-            {
-                if (tag != "Snake")
-                    player.HitByBullet();
-                else
-                {
-                    player.StopAllCoroutines();
-                    player.FreezePlayer();
-                }
-
-                Destroy(gameObject);
-            }
         }
     }
 }
