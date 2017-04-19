01e61c4fa299db94d693fecf6781bce3986b8c34
Alexandre MARIE <alexandre_marie@outlook.com>
2017-04-19 17:39:23 +0200

    Ajout des powerups + physique player corrigée

---
+++ Unity/Assets/_scripts/AddLife.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AddLife : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Horsemen player = other.GetComponent<Horsemen>();
+        if (player != null)
+        {
+            player.Life += (int)(Random.Range(1, 3) * 10f);
+        }
+
+        //Destroy(this.gameObject);
+    }
+}
---
+++ Unity/Assets/_scripts/UpdateSpecial.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateSpecial : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Horsemen player = other.GetComponent<Horsemen>();
+        if (player != null)
+        {
+            player.Stamina = 100;
+        }
+
+        this.gameObject.SetActive(false);
+
+        //Destroy(this.gameObject);
+    }
+}
