5a350eec9a42e786b00b93d6ec42df5dbc9c2bb7
Alexandre MARIE <alexandre_marie@outlook.com>
2017-04-20 16:50:39 +0200

    Begin UI screen both players

--- Unity/Assets/_scripts/LifeUpdater.cs
+++ Unity/Assets/_scripts/LifeUpdater.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-public class LifeUpdater : MonoBehaviour {
+public class LifeUpdater : MonoBehaviour
+{
 
-    Image IMG_Life;
+    Image IMG_Life = null;
 
     [SerializeField]
     private Gradient lifeColor;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         IMG_Life = GetComponent<Image>();
-	}	
+    }
 
     public void UpdateLifebar(int newLife)
     {
-        IMG_Life.fillAmount = newLife / 100f;
-        IMG_Life.color = lifeColor.Evaluate(newLife / 100f);
+        if (IMG_Life != null)
+        {
+            IMG_Life.fillAmount = newLife / 100f;
+            IMG_Life.color = lifeColor.Evaluate(newLife / 100f);
+        }
     }
 }
