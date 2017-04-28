5f7464573bc658c8a3c7d2a0dcb42431188211f5
boobasparte <remy.carivenc@hotmail.fr>
2017-04-28 16:09:01 +0200

    UpdateAnim

--- Unity/Assets/_scripts/Bullets/WarSpecialBullet.cs
+++ Unity/Assets/_scripts/Bullets/WarSpecialBullet.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class WarSpecialBullet : MonoBehaviour {
 
     Transform boss;
-
+    float time;
     public enum Phase { appearance, hit, disintegration }
 
     Phase phase = Phase.appearance;
     // Use this for initialization
     void Start () {
         boss = GameObject.FindObjectOfType<BossManager>().transform;
-       
+        time = GetComponent<Animator>().GetFloat("Time");
     }
 	
 	// Update is called once per frame
@@ -23,8 +23,11 @@ public class WarSpecialBullet : MonoBehaviour {
                 phase = Phase.hit;
                 break;
             case Phase.hit:
+
                 transform.position = boss.position + new Vector3(0f, 1.5f, 0f);
-            
+                GetComponent<Animator>().enabled = true;
+                time += Time.deltaTime;
+                GetComponent<Animator>().SetFloat("Time", time);
                 break;
             case Phase.disintegration:
                 break;
