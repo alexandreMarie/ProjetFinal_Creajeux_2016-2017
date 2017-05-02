4d42d81de38953e94df9c1b25aaf6b721c2a2abf
Ichi Sumeragi <ichisumeragi@hotmail.fr>
2017-05-02 15:40:29 +0200

    Start of Malphas AI

---
+++ Unity/Assets/_scripts/AI/MalphasAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MalphasAI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject arenaWalls = null;
+
+    private Patterns Malphas;
+
+	void Start ()
+    {
+        StartCoroutine(WallMovement());
+	}
+
+    void Update ()
+    {
+	}
+
+    private IEnumerator WallMovement()
+    {
+        Vector3 originalPos = arenaWalls.transform.localPosition;
+        float newPosY = originalPos.y;
+
+        while (true)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                newPosY -= 0.5f;
+                arenaWalls.transform.localPosition = new Vector3(originalPos.x, newPosY, originalPos.z);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            yield return new WaitForSeconds(2.0f);
+
+            for (int i = 0; i < 3; i++)
+            {
+                newPosY += 0.5f;
+                arenaWalls.transform.localPosition = new Vector3(originalPos.x, newPosY, originalPos.z);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
