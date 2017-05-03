794a95b3e3f3f9e8fb19a51e9d5096cc72379bdd
Ichi Sumeragi <ichisumeragi@hotmail.fr>
2017-05-03 10:30:40 +0200

    Commit Malphas + arena

--- Unity/Assets/_scripts/AI/MalphasAI.cs
+++ Unity/Assets/_scripts/AI/MalphasAI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class MalphasAI : MonoBehaviour
 {
     [SerializeField]
+    private GameObject crow = null;
+    [SerializeField]
     private GameObject arenaWalls = null;
 
     private Patterns Malphas;
@@ -11,6 +13,7 @@ public class MalphasAI : MonoBehaviour
 	void Start ()
     {
         StartCoroutine(WallMovement());
+        StartCoroutine(Crow());
 	}
 
     void Update ()
@@ -24,23 +27,29 @@ public class MalphasAI : MonoBehaviour
 
         while (true)
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 15; i++)
             {
-                newPosY -= 0.5f;
+                newPosY -= 0.1f;
                 arenaWalls.transform.localPosition = new Vector3(originalPos.x, newPosY, originalPos.z);
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.05f);
             }
 
             yield return new WaitForSeconds(2.0f);
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 15; i++)
             {
-                newPosY += 0.5f;
+                newPosY += 0.1f;
                 arenaWalls.transform.localPosition = new Vector3(originalPos.x, newPosY, originalPos.z);
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.05f);
             }
 
             yield return new WaitForSeconds(2.0f);
         }
+    }
+
+    private IEnumerator Crow()
+    {
+        Instantiate(crow);
+        yield return new WaitForSeconds(2.0f);
     }
 }
---
+++ Unity/Assets/_scripts/Bullets/Crow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Crow : MonoBehaviour
+{
+    [SerializeField]
+    private float destroyTime = 5.0f;
+    [SerializeField]
+    [Range(1.0f, 20.0f)]
+    private float speed = 5.0f;
+
+    private NavMeshAgent crowNavMesh = null;
+    private GameManager gm = null;
+     
+	void Start ()
+    {
+        gm = GameManager.Instance;
+        crowNavMesh = GetComponent<NavMeshAgent>();
+        crowNavMesh.speed = speed;
+	}
+
+	void Update ()
+    {
+        crowNavMesh.SetDestination(GameObject.FindGameObjectsWithTag("Player")[0].transform.position);
+	}
+}
